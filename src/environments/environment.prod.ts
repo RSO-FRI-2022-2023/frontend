a615b8adf0079816684f1f544ebe2ba22e4276b4
David Trafela <divjad---@users.noreply.github.com>
2022-12-31 12:22:49 +0100

    Popravki

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     izdelkiApiUrl: 'https://www.songify.si/rso/izdelki/v1/',
-    priljubljeniIzdelkiApiUrl: 'http://localhost:8082/v1/',
+    priljubljeniIzdelkiApiUrl: 'https://www.songify.si/rso/priljubljeni-izdelki/v1/',
     kosaricaApiUrl: 'https://www.songify.si/rso/kosarica/v1',
     valuteApiUrl: 'https://www.songify.si/rso/valute/v1'
 };
